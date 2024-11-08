022c1ac7e02e975486d82ccef7167ecdf2825f81
Bliss31415 <your_email@example.com>
2024-11-08 16:47:29 +0500

    Need to add function of adding new posts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,4 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('posts/', views.posts, name='posts'),
+    path('admin/', views.posts, name='admin'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
